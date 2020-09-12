17d305a93caa38f1b181d99d7c0e38b7fa93721f
runescapejon <jonsimpletv@gmail.com>
2020-09-12 19:17:59 -0400

    Fixes Ditto/Ultra Beast being generated in pools regardless if the config is set to false. Also, in the system if it detect any UB/Ditto being added it will switch them to Pridgey

--- src/main/java/com/mcsimonflash/sponge/wondertrade/internal/Manager.java
+++ src/main/java/com/mcsimonflash/sponge/wondertrade/internal/Manager.java
@@ -1,6 +1,8 @@
 package com.mcsimonflash.sponge.wondertrade.internal;
 
+import com.google.inject.Key;
 import com.mcsimonflash.sponge.wondertrade.data.TradeEntry;
+import com.pixelmonmod.pixelmon.api.pokemon.EnumInitializeCategory;
 import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
 import com.pixelmonmod.pixelmon.api.pokemon.PokemonSpec;
 import com.pixelmonmod.pixelmon.enums.EnumSpecies;
@@ -9,10 +11,10 @@ import java.time.LocalDateTime;
 import java.util.Random;
 
 public class Manager {
-	
+
 	private static final Random RANDOM = new Random();
 	static TradeEntry[] trades;
-	
+
 	public static TradeEntry trade(TradeEntry entry) {
 		int index = RANDOM.nextInt(trades.length);
 		TradeEntry ret = trades[index];
@@ -20,18 +22,19 @@ public class Manager {
 		Config.saveTrade(index);
 		return ret;
 	}
-	
+
 	public static TradeEntry getEntry(TradeEntry entry) {
 		return entry;
-		
+
 	}
+
 	public static TradeEntry take(int index) {
 		TradeEntry entry = trades[index];
 		trades[index] = new TradeEntry(genRandomPixelmon(), Utils.ZERO_UUID, LocalDateTime.now());
 		Config.saveTrade(index);
 		return entry;
 	}
-	
+
 	public static void fillPool(boolean overwrite, boolean overwritePlayers) {
 		for (int i = 0; i < trades.length; i++) {
 			if (trades[i] == null || overwrite && (overwritePlayers || trades[i].getOwner().equals(Utils.ZERO_UUID))) {
@@ -40,13 +43,28 @@ public class Manager {
 		}
 		Config.saveAll();
 	}
-	
+
 	private static Pokemon genRandomPixelmon() {
-		EnumSpecies type = Config.legendRate != 0 && RANDOM.nextInt(Config.legendRate) == 0 ? EnumSpecies.LEGENDARY_ENUMS[RANDOM.nextInt(EnumSpecies.LEGENDARY_ENUMS.length)] : EnumSpecies.randomPoke(false);
+		EnumSpecies type = Config.legendRate != 0 && RANDOM.nextInt(Config.legendRate) == 0
+				? EnumSpecies.LEGENDARY_ENUMS[RANDOM.nextInt(EnumSpecies.LEGENDARY_ENUMS.length)]
+				: EnumSpecies.randomPoke(false);
 		PokemonSpec spec = PokemonSpec.from(type.name);
 		spec.level = RANDOM.nextInt(Config.maxLvl - Config.minLvl) + Config.minLvl;
 		spec.shiny = Config.shinyRate != 0 && RANDOM.nextInt(Config.shinyRate) == 0;
-		return spec.create();
+		Pokemon p = spec.create();
+	//made a small workaround to EnumSpecies#RandomPoke that was generating in pool regardless of config set.
+		//this should prevent it and set it to Pidgey a normal type pokemon 
+		if (p.getSpecies().isUltraBeast()) {
+			if (Config.allowultrabeast == false) {
+				p.setSpecies(EnumSpecies.Pidgey);
+			}
+		}
+		if (p.isPokemon(EnumSpecies.Ditto)) {
+			if (Config.allowDitto == false) {
+				p.setSpecies(EnumSpecies.Pidgey);
+			}
+		}
+		return p;
+
 	}
-	
 }
